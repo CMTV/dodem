077169e154c2162b0c4f2e3622800b410dffd3d1
CMTV <CMTV@users.noreply.github.com>
2020-01-09 16:52:05 +0000

    Deploy to GH Pages

--- scripts/hash.js
+++ scripts/hash.js
@@ -1,1 +1,1 @@
-var SOLVED = 5; var HASH = {"1":null,"2":null,"3":null,"4":null,"5":null};
+var SOLVED = 8; var HASH = {"1":null,"2":null,"3":null,"4":null,"5":null,"6":null,"7":null,"8":null};
---
+++ scripts/show-hide.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){var o=$(".button-show"),c=$(".block-section-actions .action--hide");o.click(function(){$(this).closest(".block-section.solution").removeClass("_hidden")}),c.click(function(){$(this).closest(".block-section.solution").addClass("_hidden")})});
