9e6d50d2c6f8c1556a6add8b97f83550a375de72
CMTV <CMTV@users.noreply.github.com>
2020-01-11 16:53:13 +0000

    Deploy to GH Pages

--- scripts/hash.js
+++ scripts/hash.js
@@ -1,1 +1,1 @@
-var SOLVED = 10; var HASH = {"1":null,"2":null,"3":null,"4":null,"5":null,"6":null,"7":null,"8":null,"9":null,"10":null};
+var SOLVED = 11; var HASH = {"1":null,"2":null,"3":null,"4":null,"5":null,"6":null,"7":null,"8":null,"9":null,"10":null,"11":null};
