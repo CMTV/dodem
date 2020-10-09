d0bd1fb2004869759fb94ad789878603dd8c106f
CMTV <cmtvpetya@gmail.com>
2020-10-09 18:10:11 +0300

    Задачи 171 и 172

--- engine/classes/Task.ts
+++ engine/classes/Task.ts
@@ -7,6 +7,14 @@ import { Solver } from "./Solver";
 import { UtilIO } from "./Util";
 import { IProtoTaskInfo } from "./Proto";
 
+export const RESERVED_FILENAMES = {
+    TASK:   'task.md',
+    HINT:   'hint.md',
+    ANSWER: 'answer.md',
+    FAQ:    'faq.md',
+    LINKS:  'links.json'
+};
+
 export interface ITaskNav
 {
     next: number|null;
