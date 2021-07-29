7f0aaa782825dd393510f47881b495ba751ba5e5
CMTV <cmtvpetya@gmail.com>
2021-07-29 18:13:57 +0300

    Тест рекламы 1

--- site/_scripts/includes/darkMagic.ts
+++ site/_scripts/includes/darkMagic.ts
@@ -7,4 +7,15 @@ window.addEventListener('load', () =>
         document.querySelectorAll('.noDarkMagic').forEach(elem => elem.classList.add('_showing'));
         document.querySelectorAll('.yesDarkMagic').forEach(elem => elem.remove());
     }
+
+    document.querySelectorAll('.darkMagicContainer').forEach(adContainer =>
+    {
+        let adElem = adContainer.querySelector('ins.adsbygoogle');
+
+        let callback = () => { if (adElem.getAttribute('data-ad-status') === 'unfilled') adContainer.remove(); }
+        callback();
+
+        let adsObserver = new MutationObserver(callback);
+        adsObserver.observe(adElem, { attributeFilter: ['data-ad-status'] });
+    });
 });
